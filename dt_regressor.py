86101a249609d90b7722512d4a565c933bfad8d2
Jansen Penido <jansen.penido@gmail.com>
2017-12-30 21:47:20 -0200

    Introducing KFold Cross Validation.

--- dt_regressor.py
+++ dt_regressor.py
@@ -6,6 +6,8 @@ Author: Jansen Penido <jansen.penido@gmail.com>
 
 import numpy as np
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import KFold
+from matplotlib import pyplot as plt
 
 
 # Get colour features and corresponding redshift targets
@@ -26,24 +28,29 @@ def median_diff(predicted, actual):
     return np.median( abs(predicted - actual) )
 
 
-# Check the prediction accuracy with median_diff
-def validate_model(model, features, targets):
-    # split the data into training and testing features and predictions
-    split = features.shape[0]//2
-    train_features = features[:split]
-    test_features = features[split:]
+# Validate predictions using KFold cross validations
+def cross_validate_predictions(model, features, targets, k):
+    kf = KFold(n_splits=k, shuffle=True)
 
-    train_targets = targets[:split]
-    test_targets = targets[split:]
+    # declare an array for predicted redshifts from each iteration
+    all_predictions = np.zeros_like(targets)
 
-    # train the model
-    model.fit(train_features, train_targets)
+    for train_indices, test_indices in kf.split(features):
+        # split the data into training and testing
+        train_features, test_features = features[train_indices], features[test_indices]
+        train_targets, test_targets = targets[train_indices], targets[test_indices]
 
-    # get the predicted_redshifts
-    predictions = model.predict(test_features)
+        # fit the model for the current set
+        model.fit(train_features, train_targets)
 
-    # use median_diff function to calculate the accuracy
-    return median_diff(test_targets, predictions)
+        # predict using the model
+        predictions = model.predict(test_features)
+
+        # put the predicted values in the all_predictions array defined above
+        all_predictions[test_indices] = predictions
+
+    # return the predictions
+    return all_predictions
 
 
 if __name__ == "__main__":
@@ -53,6 +60,17 @@ if __name__ == "__main__":
     # initialize model
     dtr = DecisionTreeRegressor()
 
-    # validate the model and print the med_diff
-    diff = validate_model(dtr, features, targets)
-    print('Median difference: {:f}'.format(diff))
+    # call cross validation function
+    predictions = cross_validate_predictions(dtr, features, targets, 10)
+
+    # calculate and print the model's accuracy using median difference
+    diffs = median_diff(predictions, targets)
+    print('Median difference: {:.3f}'.format(diffs))
+
+    # plot the model's results
+    plt.scatter(targets, predictions, s=0.4)
+    plt.xlim((0, targets.max()))
+    plt.ylim((0, predictions.max()))
+    plt.xlabel('Measured Redshift')
+    plt.ylabel('Predicted Redshift')
+    plt.show()
